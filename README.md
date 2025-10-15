3fc7c5b1cce39ecf477e12a69da05ac417b40f7d
Steven <13852721+spmvg@users.noreply.github.com>
2025-10-15 21:34:36 +0200

    Hello world with a ROS package (#3)
    * run image [spmvg/echo#1]
    
    * mount [spmvg/echo#1]
    
    * package [spmvg/echo#1]
    
    * info [spmvg/echo#1]
    
    * node [spmvg/echo#1]
    
    * build [spmvg/echo#1]

---
+++ README.md
@@ -0,0 +1,25 @@
+# Echo
+Interactive voice chat system.
+
+## Development setup
+Build the container:
+```shell
+docker build -t echo -f docker/.Dockerfile .
+```
+
+Run the container:
+```shell
+docker run -it -v $(pwd -W)/ros2_workspace:/root/ros2_workspace echo
+```
+
+Go to the workspace directory and build the package:
+```shell
+cd /root/ros2_workspace
+colcon build
+source install/local_setup.bash
+```
+
+Run the package:
+```shell
+ros2 run echo echo_node
+```
---
+++ ros2_workspace/src/echo/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'echo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Steven',
+    maintainer_email='steven@vangemert.dev',
+    description='Interactive voice chat system',
+    license='AGPL-3.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'echo_node = echo.echo_node:main'
+        ],
+    },
+)
