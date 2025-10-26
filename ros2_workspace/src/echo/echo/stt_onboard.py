9275234e5fb598806d157b1eda309a8d64e51e72
spmvg <13852721+spmvg@users.noreply.github.com>
2025-10-26 19:17:40 +0100

    wake word detection and recording the audio [spmvg/echo#1_2]

--- ros2_workspace/src/echo/echo/stt_onboard.py
+++ ros2_workspace/src/echo/echo/stt_onboard.py
@@ -1,6 +1,9 @@
+import os
 import threading
+from collections import deque
 from queue import Queue
 import wave
+from time import time
 
 import rclpy
 from rclpy.node import Node
@@ -9,13 +12,18 @@ from std_msgs.msg import String
 import sounddevice
 
 try:
-    from pocketsphinx import Pocketsphinx
+    import pocketsphinx
+    from pocketsphinx import Pocketsphinx, Decoder
 except Exception as e:
     raise RuntimeError(
         "Failed to import pocketsphinx. Make sure 'pocketsphinx' is installed.\n"
         "On Debian/Ubuntu: sudo apt install swig build-essential portaudio19-dev\n"
         "then pip install pocketsphinx pyaudio"
     ) from e
+
+
+WAKE_WORD_MODE = "wakeword"
+LANGUAGE_SEARCH_MODE = "lm"
 
 
 class STTOnboard(Node):
@@ -37,26 +45,23 @@ class STTOnboard(Node):
             buffer_size: int = 1024,
             channels: int = 1,
             rate: int = 16000,
+            wake_word: str = "computer",
+            time_to_wait_in_silence_seconds: float = 2.0,
+            max_listen_time_seconds: float = 15.0,
     ):
         """
-        Note: pocketsphinx might register some false positives when starting up. This becomes less after some actual
-        speech is detected.
+        Uses PocketSphinx's built-in keyword spotting to listen for a wake word.
+        Once detected, starts recording audio until silence and saves it to a .wav file.
         """
-        pocketsphinx = Pocketsphinx()
-
-        # Queue to collect audio data from the callback
-        audio_q = Queue()
+        audio_q = Queue()  # TODO: queue is probably not FILO, consider using deque
+        frames = []
 
         def audio_callback(indata, frames, time, status):
-            """Callback called from sounddevice every audio block."""
             audio_q.put(indata.copy())
 
-        # Start PocketSphinx in utterance mode
-        pocketsphinx.start_utt()
-        frames = []
-        phrase_index = 0
+        self.get_logger().info(f"Listening for wake word '{wake_word}'...")
 
-        self.get_logger().info("STT listening...")
+        decoder = make_decoder(wake_word)
 
         with sounddevice.InputStream(
                 samplerate=rate,
@@ -65,51 +70,76 @@ class STTOnboard(Node):
                 blocksize=buffer_size,
                 callback=audio_callback,
         ):
+            decoder.start_utt()
+            mode = WAKE_WORD_MODE
+
             while True:
-                # Get audio chunk from queue
-                data = audio_q.get()
-                raw = data.tobytes()
-                frames.append(raw)
-                pocketsphinx.process_raw(raw, False, False)
+                raw = audio_q.get().tobytes()
+                decoder.process_raw(raw, False, False)
 
-                hyp = pocketsphinx.hyp()
-                if hyp is not None:
-                    self.get_logger().info(f"Recognized: {hyp.hypstr}")
+                hyp = decoder.hyp()
 
-                    # --- Save audio for this phrase ---
-                    wav_filename = f"phrase_{phrase_index}.wav"
-                    with wave.open(wav_filename, "wb") as wf:
-                        wf.setnchannels(channels)
-                        wf.setsampwidth(2)  # int16 -> 2 bytes
-                        wf.setframerate(rate)
-                        wf.writeframes(b"".join(frames))
-                    self.get_logger().info(f"Saved audio to {wav_filename}\n")
-
-                    # Reset for next utterance
+                if mode == WAKE_WORD_MODE and hyp:
+                    self.get_logger().info("Wake word detected!")
+                    mode = LANGUAGE_SEARCH_MODE
+                    decoder.end_utt()
+                    decoder.set_search(LANGUAGE_SEARCH_MODE)
+                    decoder.start_utt()
                     frames = []
-                    pocketsphinx.end_utt()
-                    pocketsphinx.start_utt()
-                    phrase_index += 1
-        #
-        # except KeyboardInterrupt:
-        #     print("\nStopping...")
-        # finally:
-        #     pocketsphinx.end_utt()
+                    speech, speech_updated, start_time = None, time(), time()
+                    continue
+                elif mode == WAKE_WORD_MODE:
+                    continue
+                elif mode == LANGUAGE_SEARCH_MODE:
+                    frames.append(raw)
 
-        # The LiveSpeech iterator blocks and yields recognized chunks.
-        # for phrase in self.speech:
-        #     try:
-        #         text = str(phrase).strip()
-        #         if not text:
-        #             continue
-        #         msg = String()
-        #         msg.data = text
-        #         # publish on the ROS topic
-        #         self.pub.publish(msg)
-        #         self.get_logger().info(f"Recognized: {text}")
-        #     except Exception as e:
-        #         self.get_logger().error(f"Error during recognition loop: {e}")
+                    if not hyp:
+                        continue  # no speech yet
 
+                    new_text = hyp.hypstr.lower()
+                    if speech != new_text:
+                        speech, speech_updated = new_text, time()
+                        continue
+
+                    if (
+                            (time() - speech_updated > time_to_wait_in_silence_seconds)
+                            or (time() - start_time > max_listen_time_seconds)
+                    ):
+                        # Consider utterance complete after a period of no change
+                        decoder.end_utt()
+                        with wave.open('phrase.wav', "wb") as wf:
+                            wf.setnchannels(channels)
+                            wf.setsampwidth(2)
+                            wf.setframerate(rate)
+                            wf.writeframes(b"".join(frames))
+                        self.get_logger().info(f"Phrase is complete. Switching back to wake word detection.")
+
+                        # Reset for wake word detection
+                        decoder.set_search(WAKE_WORD_MODE)
+                        decoder.start_utt()
+                        mode = WAKE_WORD_MODE
+                        frames = []
+                else:
+                    raise RuntimeError("Unknown mode in STTOnboard listen loop")
+
+
+def make_decoder(wake_word: str) -> Decoder:
+    model_path = os.path.join(os.path.dirname(pocketsphinx.__file__), 'model', 'en-us')
+
+    config = Decoder.default_config()
+    config.set_string('-hmm', os.path.join(model_path, 'en-us'))
+    config.set_string('-dict', os.path.join(model_path, 'cmudict-en-us.dict'))
+    config.set_string('-lm', os.path.join(model_path, 'en-us.lm.bin'))
+    decoder = Decoder(config)
+
+    # --- Add wake word search ---
+    decoder.set_keyphrase(WAKE_WORD_MODE, wake_word)
+    decoder.set_search(WAKE_WORD_MODE)
+
+    # --- Add language model search ---
+    decoder.set_lm_file('lm', os.path.join(model_path, 'en-us.lm.bin'))
+
+    return decoder
 
 def main(args=None):
     rclpy.init(args=args)
