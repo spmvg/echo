78785b74430e63d7fdeb15797536dc7cfcaddd47
spmvg <13852721+spmvg@users.noreply.github.com>
2025-12-31 18:15:19 +0100

    make extra log for debuggability [spmvg/echo#17_log]

--- ros2_workspace/src/echo/echo/stt_onboard.py
+++ ros2_workspace/src/echo/echo/stt_onboard.py
@@ -68,6 +68,8 @@ class STTOnboard(Node):
 
         decoder = make_decoder(wake_word)
 
+        self.get_logger().info(str(sounddevice.query_devices()))
+
         with sounddevice.InputStream(
                 samplerate=rate,
                 channels=channels,
